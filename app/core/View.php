b22853e85b8f7e84e1e48d7efd4449404bffaa79
Tawdi <ahmedtawdi00@gmail.com>
2025-02-07 16:16:04 +0100

    ajoute class view (Core)

--- app/core/View.php
+++ app/core/View.php
@@ -2,7 +2,23 @@
 
 namespace app\core;
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 class View
 {
-    
+    private static $twig;
+
+    public static function getTwig()
+    {
+        if (!self::$twig) {
+            
+            $loader = new FilesystemLoader(__DIR__ . '/../views'); 
+
+            self::$twig = new Environment($loader);
+        }
+
+        return self::$twig;
+    }
 }
+
