0a0944bb342020cc75b18248580976111cd14ec1
Tawdi <ahmedtawdi00@gmail.com>
2025-02-07 09:15:32 +0100

    ajoute Model Class

--- app/core/Model.php
+++ app/core/Model.php
@@ -2,7 +2,30 @@
 
 namespace app\core;
 
-class Model
+
+abstract class Model
 {
+
+    public function __get($property)
+    {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
+        return null; 
+    }
+
+    public function __set($property, $value)
+    {
+        if (property_exists($this, $property)) {
+            $this->$property = $value;
+        }
+    }
+
+    abstract public function create() ;
     
+    abstract public function read() ;
+    
+    abstract public function update() ;
+    
+    abstract public function delete() ;
 }
